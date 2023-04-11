aa43b276491c19d8d63dece78d54238661e650c8
WelberLopes <deunatelhacontato01@gmail.com>
2023-04-10 21:51:06 -0300

    Imprimindo valores

--- lojalatas/src/App.java
+++ lojalatas/src/App.java
@@ -22,11 +22,32 @@ public class App {
         double ValortotalGaloes = qtdgaloes * precogalão;
 
         double m2Mistura = m2 * 1.1;
+        double qtdLatasMis = m2Mistura / (capacidadeLatas * rendimento);
+        double qtdLatasMisArredondada = Math.ceil(m2Mistura / (capacidadeLatas * rendimento));
+        double qtddisperdicioLatas = qtdLatasMisArredondada - qtdLatasMis;
+        double qtdConvertidaemGaloes = (1 - qtddisperdicioLatas) * capacidadeLatas;
+        double quantidadeEmGaloesArredondada = Math.ceil(qtdConvertidaemGaloes / capacidadegalao);
+        double qtdFinalmisturalata = Math.floor(qtdLatasMis);
+
+        double ValorMisGalão = quantidadeEmGaloesArredondada * precogalão;
+        double ValorMisLata = qtdFinalmisturalata * precolata;
+
+        double valortotalMis = ValorMisGalão + ValorMisLata;
 
         System.out.printf("A quantidade de latas a serem utilizadas será %f latas e custará %f reais.", qtdLatas,
                 ValorTotalLatas);
         System.out.printf("\n" + "A quantidade de galões a serem utilizados será %f  e custará %f reais.", qtdgaloes,
                 ValortotalGaloes);
+        System.out.printf(
+                "\n" + "A quantidade de latas a serem utilizados  na mistura será %f  e %f  galões e custará %f reais.",
+                qtdFinalmisturalata, quantidadeEmGaloesArredondada, valortotalMis);
+        System.out.println("\n");
+        System.out.println(qtdLatasMisArredondada);
+        System.out.println(m2Mistura);
+        System.out.println(qtdLatasMis);
+        System.out.println(qtdFinalmisturalata);
+        System.out.println(qtdConvertidaemGaloes);
+        System.out.println(quantidadeEmGaloesArredondada);
 
     }
 }
